51fc3718dc11ce5920c63268d2d95fd4c86a6d68
Hani Mir <hmir95@gmail.com>
2019-03-05 19:26:26 -0500

    Added module comment

--- classes/query.py
+++ classes/query.py
@@ -1,3 +1,23 @@
+"""
+This module implements a query container, which stores the tables used by the query,
+as well as the query itself.
+
+A query is evaluated as follows:
+1) All conditions (which come from the "where" clause of the SQL query) which only
+   affect one table are evaluated, and the corresponding tables are updated.
+2) All tables are joined together.
+3) The remaining conditions (those which affect multiple tables) are evaluated.
+4) The desired columns are selected according to the "select" clause of the SQL query.
+
+The reason why single-table conditions are evaluated first is to reduce the size
+of the tables that will be joined in step 2. The most expensive operation in the
+evaluation of a query is the cross-product of the data tables. The cross-product's
+runtime scales with the number of rows per table, so filtering the tables before
+step 2 has the potential to significantly improve performance compared to the naive
+cross-product approach.
+"""
+
+
 import json
 import os
 
